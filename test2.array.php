d0087baa8799927868a30f3b94df6eb56b5a93d3
villabajo <m@tty0.ru>
2015-02-28 14:20:33 +0000

    commit 3

--- test2.array.php
+++ test2.array.php
@@ -1,38 +1,38 @@
 <?php
-    srand();
-    $myArray=array();
-    $myArraySize=rand(0,100);
-    echo "Size of Array: " . $myArraySize . "<br>";
-    $i=0;
-    for($i=0; $i<$myArraySize; $i++){
-        $myArray[$i]=rand(-100,100);
-        // echo $i . ": " . $myArray[$i] . " ";
+srand();
+$myArray=array();
+$myArraySize=rand(0,100);
+echo "Size of Array: " . $myArraySize . "<br>";
+$i=0;
+for($i=0; $i<$myArraySize; $i++){
+    $myArray[$i]=rand(-100,100);
+    // echo $i . ": " . $myArray[$i] . " ";
+}
+
+// echo "<br>";
+
+$maxSequence=array();
+$maxSize=0;
+$j=0;
+
+for($i=0; $i<$myArraySize; $i++){
+    echo $myArray[$i] . " ";
+    if($myArray[$i]>0){
+        $temp[$j]=$myArray[$i];
+        $j++;
     }
-    
-    // echo "<br>";
-    
-    $maxSequence=array();
-    $maxSize=0;
-    $j=0;
-    
-    for($i=0; $i<$myArraySize; $i++){
-        echo $myArray[$i] . " ";
-        if($myArray[$i]>0){
-            $temp[$j]=$myArray[$i];
-            $j++;
+    else{
+        if($maxSize<=$j){
+            $maxSize=$j;
+            $maxSequence=$temp;
+            // print_r($temp);
         }
-        else{
-            if($maxSize<=$j){
-                $maxSize=$j;
-                $maxSequence=$temp;
-                // print_r($temp);
-            }
-            $j=0;
-            $temp=[];
-            echo "<br>";
-        }
+        $j=0;
+        $temp=[];
+        echo "<br>";
     }
-    
-    echo "<br> Max Length of positive sequence: " . $maxSize . "<br>";
-    print_r($maxSequence);
+}
+
+echo "<br> Max Length of positive sequence: " . $maxSize . "<br>";
+print_r($maxSequence);
 ?>
